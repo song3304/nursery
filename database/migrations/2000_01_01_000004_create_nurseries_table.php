caf7bfbd769f032a556fdb8acbb6d5483dc0f3fd
songhui <284606700@qq.com>
2018-04-09 20:45:51 +0800

    修改

--- database/migrations/2000_01_01_000004_create_nurseries_table.php
+++ database/migrations/2000_01_01_000004_create_nurseries_table.php
@@ -2,7 +2,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateWatersTable extends Migration
+class CreateNurseriesTable extends Migration
 {
 	/**
 	 * Run the migrations.
